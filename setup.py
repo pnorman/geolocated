feb4cdc15a2c5e33f3b20e40e57b633c04f59d5f
Paul Norman <penorman@mac.com>
2014-06-21 13:07:28 -0700

    Add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+
+  name = "geolocated",
+  version = "0.0.1",
+  description = "Geotagger for images, from GPX",
+  author = "Paul Norman",
+  author_email = "penorman@mac.com",
+  url = "https://github.com/pnorman/geolocated"
+)
